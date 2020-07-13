618414b6d5fd5b1d08b7dfda9de926ea75dadc6f
deleonalejandro <54589673+deleonalejandro@users.noreply.github.com>
2020-07-13 16:48:01 -0500

    Ultimos cambios

--- src/main/java/com/canfer/app/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.canfer.app;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeeRepository extends JpaRepository<Employee, Long>{
-
-}
