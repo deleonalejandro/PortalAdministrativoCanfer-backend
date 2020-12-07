0294cfd96f975f319e5822b68f2d88ebaffed9aa
deleonalejandro <54589673+deleonalejandro@users.noreply.github.com>
2020-12-07 17:33:10 -0600

    Clase Documento
    Preparacion para la version 2.0

--- src/main/java/com/canfer/app/model/Documento.java
+++ src/main/java/com/canfer/app/model/Documento.java
@@ -6,15 +6,13 @@ import java.time.LocalDateTime;
 
 import javax.persistence.Column;
 import javax.persistence.DiscriminatorColumn;
-import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+
 
 import org.hibernate.annotations.CreationTimestamp;
 
@@ -28,113 +26,42 @@ public class Documento {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long idDocumento;
 	
-	private Long idTabla;
+	private Archivo archivoXML;
 	
-	@JoinColumn(name = "idEmpresa")
-	@ManyToOne
-	private Empresa empresa;
+	private Archivo archivoPDF;
+	
+	@Column(nullable = false)
+	@CreationTimestamp
+	private LocalDateTime fechaCreacion;
+	
+	private LocalDateTime fechaMod;
 	
 	private String objEntry;
 	
 	private String docEntry;
 	
-	@Column(nullable = false)
-	private String modulo;
 	
-	@Column(nullable = false)
-	private String concepto;
-	
-	@Column(nullable = false)
-	@CreationTimestamp
-	private LocalDateTime fecha;
-	
-	@Column(nullable = false)
-	private String extension;
-	
-	@Column(nullable = false)
-	private String ruta;
 
 	//Constructor
 	
 	public Documento() {
-		super();
+		
 	}
 	
-	public Documento(Long idTabla, Empresa empresa, String modulo, String concepto, String extension, String ruta) {
-		super();
-		this.idTabla = idTabla;
-		this.empresa = empresa;
-		this.modulo = modulo;
-		this.concepto = concepto;
-		this.extension = extension;
-		this.ruta = ruta;
+	public Documento(Archivo archivoXML, Archivo archivoPDF) {
+		this.archivoXML = archivoXML;
+		this.archivoPDF = archivoPDF;
+			
 	}
 
 
 	//Getters and Setters
-
 	public Long getIdDocumento() {
 		return idDocumento;
 	}
 
 	public void setIdDocumento(Long idDocumento) {
 		this.idDocumento = idDocumento;
-	}
-
-	public String getModulo() {
-		return modulo;
-	}
-
-	public void setModulo(String modulo) {
-		this.modulo = modulo;
-	}
-
-	public String getConcepto() {
-		return concepto;
-	}
-
-	public void setConcepto(String concepto) {
-		this.concepto = concepto;
-	}
-
-	public LocalDateTime getFecha() {
-		return fecha;
-	}
-
-	public void setFecha(LocalDateTime fecha) {
-		this.fecha = fecha;
-	}
-
-	public String getExtension() {
-		return extension;
-	}
-
-	public void setExtension(String extension) {
-		this.extension = extension;
-	}
-
-	public String getRuta() {
-		return ruta;
-	}
-
-	public void setRuta(String ruta) {
-		this.ruta = ruta;
-	}
-
-	public Long getIdTabla() {
-		return idTabla;
-	}
-
-	public void setIdTabla(Long idTabla) {
-		this.idTabla = idTabla;
-	}
-
-	public Empresa getEmpresa() {
-		return empresa;
-	}
-
-	public void setEmpresa(Empresa empresa) {
-		this.empresa = empresa;
 	}
 
 	public String getObjEntry() {
@@ -152,33 +79,43 @@ public class Documento {
 	public void setDocEntry(String docEntry) {
 		this.docEntry = docEntry;
 	}
-	
-	@Entity
-	@DiscriminatorValue("DOCUMENTO_XML")
-	public static class DocumentoXML extends Documento {
-		
-		public DocumentoXML() {
-			// Default constructor
-		}
-		
-		public DocumentoXML(Long idTabla, Empresa empresa, String modulo, String concepto, String extension, String ruta) {
-			super(idTabla, empresa, modulo, concepto, extension, ruta);
-		}
+
+	public Archivo getArchivoXML() {
+		return archivoXML;
+	}
+
+	public void setArchivoXML(Archivo archivoXML) {
+		this.archivoXML = archivoXML;
+	}
+
+	public Archivo getArchivoPDF() {
+		return archivoPDF;
+	}
+
+	public void setArchivoPDF(Archivo archivoPDF) {
+		this.archivoPDF = archivoPDF;
+	}
+
+	public LocalDateTime getFechaCreacion() {
+		return fechaCreacion;
+	}
+
+	public void setFechaCreacion(LocalDateTime fechaCreacion) {
+		this.fechaCreacion = fechaCreacion;
+	}
+
+	public LocalDateTime getFechaMod() {
+		return fechaMod;
+	}
+
+	public void setFechaMod(LocalDateTime fechaMod) {
+		this.fechaMod = fechaMod;
 	}
 	
-	@Entity
-	@DiscriminatorValue("DOCUMENTO_PDF")
-	public static class DocumentoPDF extends Documento {
-		
-		public DocumentoPDF() {
-			// Default constructor
-		}
-		
-		public DocumentoPDF(Long idTabla, Empresa empresa, String modulo, String concepto, String extension, String ruta) {
-			super(idTabla, empresa, modulo, concepto, extension, ruta);
-		}
-	}
-	
+	//TODO 
+	public void save() {}
+	public void delete() {}
+	public void download() {}
 	
 	
 	
