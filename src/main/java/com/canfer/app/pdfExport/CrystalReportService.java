7d9babe38b5911d890482741b8f12e23e20bbc1f
deleonalejandro <54589673+deleonalejandro@users.noreply.github.com>
2020-12-09 20:15:42 -0600

    Crystal Arreglado

--- src/main/java/com/canfer/app/pdfExport/CrystalReportService.java
+++ src/main/java/com/canfer/app/pdfExport/CrystalReportService.java
@@ -1,3 +1,4 @@
+package com.canfer.app.pdfExport;
 
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.WriterException;
@@ -10,7 +11,6 @@ import java.nio.file.FileSystems;
 import java.nio.file.Path;
 
 import com.canfer.app.cfd.Comprobante;
-import com.canfer.app.model.Archivo;
 import com.canfer.app.model.Archivo.ArchivoPDF;
 import com.canfer.app.model.ComprobanteFiscal;
 import com.canfer.app.model.Documento;
