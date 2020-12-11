15793d38cb6763198cdf108fc7062e89491223ab
yasminfemerling <yasminfemerling@users.noreply.github.com>
2020-12-11 07:56:00 -0600

    Update Links

--- src/main/java/com/canfer/app/controller/DocumentosNacionalesFunctionalityController.java
+++ src/main/java/com/canfer/app/controller/DocumentosNacionalesFunctionalityController.java
@@ -85,7 +85,7 @@ public class DocumentosNacionalesFunctionalityController {
 	
 	}
 	
-	@GetMapping("/preview/{repo}/{id}")
+	@GetMapping("/preview/{method}/{repo}/{id}")
 	public ResponseEntity<Resource> preview(@PathVariable Long id, @PathVariable String method, @PathVariable String repo) {
 		
 		return actioner.download(method, repo, id, "p");
