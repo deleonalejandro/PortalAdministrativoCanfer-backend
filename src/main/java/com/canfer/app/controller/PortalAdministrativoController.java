6c14ff93bd4accaf55581565540930bc3c68e38f
yasminfemerling <yasminfemerling@users.noreply.github.com>
2020-12-15 13:28:48 -0600

    quitar cookie de controller

--- src/main/java/com/canfer/app/controller/PortalAdministrativoController.java
+++ src/main/java/com/canfer/app/controller/PortalAdministrativoController.java
@@ -74,13 +74,6 @@ public class PortalAdministrativoController {
 			return "redirect:/dashboard";
 		}
 
-		
-		
-		/*
-		//create cookie
-		Cookie empresaCookie = new Cookie("empresa", idEmpresa);
-		//add cookie
-		response.addCookie(empresaCookie);*/
 	}
 	
 	@GetMapping(value = "/dashboard")
