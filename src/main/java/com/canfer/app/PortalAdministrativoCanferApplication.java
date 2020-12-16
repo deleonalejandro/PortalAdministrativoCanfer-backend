38e12600256bfe3f7521ca5c83ef62e127e923f7
yasminfemerling <yasminfemerling@users.noreply.github.com>
2020-12-16 00:59:13 -0600

    applications.yml

--- src/main/java/com/canfer/app/PortalAdministrativoCanferApplication.java
+++ src/main/java/com/canfer/app/PortalAdministrativoCanferApplication.java
@@ -7,9 +7,10 @@ package com.canfer.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class PortalAdministrativoCanferApplication {
 		
 	public static void main(String[] args) {
