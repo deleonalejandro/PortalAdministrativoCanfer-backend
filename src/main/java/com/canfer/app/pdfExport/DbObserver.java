9448f033cd358b1027b5c38875560c14dd352be5
deleonalejandro <54589673+deleonalejandro@users.noreply.github.com>
2021-01-04 18:19:42 -0600

    User and password into application properties for Payments DB

--- src/main/java/com/canfer/app/pdfExport/DbObserver.java
+++ src/main/java/com/canfer/app/pdfExport/DbObserver.java
@@ -3,6 +3,7 @@ package com.canfer.app.pdfExport;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.canfer.app.mail.EmailSenderService;
@@ -29,10 +30,12 @@ public class DbObserver {
 	@Autowired 
 	CrystalReportService crService; 
 	
+	@Value("${canfer.pagos.username}")
+	private String user;
+	@Value("${canfer.pagos.password}")
+	private String psswd;
+	
 	public void checkPago() {
-
-		String user = "sa"; 
-		String psswd = "q2y72-m9t9q"; 
 		
 		//Busca pagos que no hayan sido procesados
 		List<Pago> pagos = pagoRepository.findByBitProcesadoAndNuevoEstatusFactura(false,"Pagado");
@@ -43,12 +46,6 @@ public class DbObserver {
 
 			//TODO poner la contraseña en un lugar seguro
 			ArchivoPDF exportedFile = crService.exportPDF(pago, user, psswd);
-			
-			if (exportedFile == null) {
-				
-				Log.falla("No se pudo exportar el pago "+ pago.getIdNumPago()+ " a PDF.", "ERROR");
-				
-			}
 			
 			//Guardamos el bit procesado en la bd
 			pago.setBitProcesado(true);
