eb9124f2337abe9e741de74aa45d2e0eec1c2628
deleonalejandro <54589673+deleonalejandro@users.noreply.github.com>
2021-01-25 18:17:17 -0600

    Cookies cleared before changing company
    Filters not preserved between companies.

--- src/main/resources/static/js/tabledf.js
+++ src/main/resources/static/js/tabledf.js
@@ -558,6 +558,32 @@
 		
 			});
 			
+			// Clear cookies before when changing company
+			$("#dashboardButton").on( "click", function() {
+				
+				Cookies.remove('fltr_on');
+				
+				Cookies.remove('fltr_proveedor');
+				Cookies.remove('fltr_idNumSap');
+				Cookies.remove('fltr_uuid');
+				Cookies.remove('fltr_estatusPago');
+				Cookies.remove('fltr_uploadAfter');
+				Cookies.remove('fltr_uploadBefore');
+				Cookies.remove('fltr_upload');
+				Cookies.remove('fltr_registeredAfter');
+				Cookies.remove('fltr_registeredBefore');
+				Cookies.remove('fltr_register');
+				Cookies.remove('fltr_sequenceAfter');
+				Cookies.remove('fltr_sequenceBefore');
+				Cookies.remove('fltr_totalAfter');
+				Cookies.remove('fltr_totalBefore');
+				Cookies.remove('fltr_generico');
+				Cookies.remove('fltr_checkSap');
+				Cookies.remove('fltr_hasComplemento');
+				
+				
+			});
+			
 			// Clear filters
 			$('#clearFilters').on('click', function() { 
 				
@@ -991,6 +1017,8 @@
 			if($("#deletePermission").text() == 'false') {
 		          $('#toastDeletefalse').toast('show')
 		     }
+
+			
 				
 	});
 	
