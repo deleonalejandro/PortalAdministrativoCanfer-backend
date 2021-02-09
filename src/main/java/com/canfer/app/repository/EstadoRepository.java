de35731b818ac0ba77459b6dbf129192262ed452
deleonalejandro <54589673+deleonalejandro@users.noreply.github.com>
2021-02-09 15:58:35 -0600

    Test SP for EstadoRepo
    insertEstado for testing SP

--- src/main/java/com/canfer/app/repository/EstadoRepository.java
+++ src/main/java/com/canfer/app/repository/EstadoRepository.java
@@ -1,10 +1,15 @@
 package com.canfer.app.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.query.Procedure;
 
 import com.canfer.app.model.Estado;
 
 public interface EstadoRepository extends JpaRepository<Estado, Long>{
+	
+	// this function was created to test SP
+	@Procedure("insertEstado")
+	void agregaEstado(String pNombre);
 
 }
 
