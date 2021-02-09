1909ddbd953c579d0d6d6bb0039dd0993800c397
deleonalejandro <54589673+deleonalejandro@users.noreply.github.com>
2021-02-09 16:01:45 -0600

    ProveedorRepo SP
    actualiza proveedores store procedure

--- src/main/java/com/canfer/app/repository/ProveedorRepository.java
+++ src/main/java/com/canfer/app/repository/ProveedorRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.query.Procedure;
 
 import com.canfer.app.model.Empresa;
 import com.canfer.app.model.Proveedor;
@@ -25,6 +26,9 @@ public interface ProveedorRepository extends JpaRepository<Proveedor, Long> {
 	Proveedor findByEmpresasAndNombre(Empresa empresa, String nombre);
 	
 	List<Proveedor> findAllByEmpresas(Empresa empresa);
+	
+	@Procedure("sp_PortalProveedores_ActualizaProveedores")
+	void updateSuppliersFromSap(String pServidor, String pBaseDatos, Long pId_Empresa);
 	
 
 
