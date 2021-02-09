27cad55fd8bf4da76efe0a5351318d42d48d2c3b
arochacanfer <69600070+arochacanfer@users.noreply.github.com>
2021-02-09 16:09:06 -0600

    Caja chica updates al modal de nuevo formulario

--- src/main/java/com/canfer/app/repository/FormularioCajaChicaRepository.java
+++ src/main/java/com/canfer/app/repository/FormularioCajaChicaRepository.java
@@ -1,10 +1,16 @@
 package com.canfer.app.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.canfer.app.model.ComprobanteFiscal;
 import com.canfer.app.model.FormularioCajaChica;
 
 public interface FormularioCajaChicaRepository extends JpaRepository<FormularioCajaChica, Long>{
+
+	List<ComprobanteFiscal> findAll(Specification<ComprobanteFiscal> comprobanteSpec);
 
 }
 
